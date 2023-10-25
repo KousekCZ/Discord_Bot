76733b79fda620d418b1193d7715e60f0deef394
KousekCZ <105644565+KousekCZ@users.noreply.github.com>
2023-10-25 08:52:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -38,44 +38,43 @@ async def on_message(message): # message.author.mention
                 if command in content:
                     command_found = True
 
-                    match command:
-                        case "help":
-                            await message.channel.send(
-                                '__# Seznam příkazů:__\npro aktivaci napiš @Kusovník [název příkazu]\n\n- help => zobrazí seznam příkazů\n- ahoj => vypíše uvítací zprávu')
-                            await target_channel.send(f"```diff\n+ {date_time} - Na serveru '{server}' byl použit příkaz '{command}'.\n```")
+                    if command == "help":
+                        await message.channel.send(
+                            '__# Seznam příkazů:__\npro aktivaci napiš @Kusovník [název příkazu]\n\n- help => zobrazí seznam příkazů\n- ahoj => vypíše uvítací zprávu')
+                        await target_channel.send(f"```diff\n+ {date_time} - Na serveru '{server}' byl použit příkaz '{command}'.\n```")
 
-                        case "ahoj":
-                            await message.channel.send(f'{message.author.mention} Co mě pinguješ more...')
-                            await target_channel.send(f"```diff\n+ {date_time} - Na serveru '{server}' byl použit příkaz '{command}'.\n```")
+                    elif command == "ahoj":
+                        await message.channel.send(f'{message.author.mention} Co mě pinguješ more...')
+                        await target_channel.send(f"```diff\n+ {date_time} - Na serveru '{server}' byl použit příkaz '{command}'.\n```")
 
-                        case "pingg":
-                            for i in range(3):
-                                await message.channel.send("@everyone")
-                            await target_channel.send(f"```diff\n+ {date_time} - Na serveru '{server}' byl použit příkaz '{command}'.\n```")
+                    elif command == "pingg":
+                        for i in range(3):
+                            await message.channel.send("@everyone")
+                        await target_channel.send(f"```diff\n+ {date_time} - Na serveru '{server}' byl použit příkaz '{command}'.\n```")
 
-                        case "smaž":
-                            await target_channel.send(f"```diff\n+ {date_time} - Na serveru '{server}' byl použit příkaz '{command}'.\n```")
-                            try:
-                                words = content.split()
-                                smaz_index = words.index("smaž")
-                                num_to_delete = int(words[smaz_index + 1])
-                                messages_to_delete = []
+                    elif command == "smaž":
+                        await target_channel.send(f"```diff\n+ {date_time} - Na serveru '{server}' byl použit příkaz '{command}'.\n```")
+                        try:
+                            words = content.split()
+                            smaz_index = words.index("smaž")
+                            num_to_delete = int(words[smaz_index + 1])
+                            messages_to_delete = []
 
-                                async for previous_message in message.channel.history(limit=num_to_delete + 1):
-                                    messages_to_delete.append(previous_message)
+                            async for previous_message in message.channel.history(limit=num_to_delete + 1):
+                                messages_to_delete.append(previous_message)
 
-                                if messages_to_delete:
-                                    await message.channel.delete_messages(messages_to_delete)
+                            if messages_to_delete:
+                                await message.channel.delete_messages(messages_to_delete)
 
-                                    result_message = await message.channel.send(f"Smazal jsem {num_to_delete} zpráv...")
+                                result_message = await message.channel.send(f"Smazal jsem {num_to_delete} zpráv...")
 
-                                    await asyncio.sleep(2)
-                                    await result_message.delete()
-                                else:
-                                    await message.channel.send("Nemohu najít zprávy ke smazání.")
+                                await asyncio.sleep(2)
+                                await result_message.delete()
+                            else:
+                                await message.channel.send("Nemohu najít zprávy ke smazání.")
 
-                            except (ValueError, IndexError):
-                                await message.channel.send("Nemohu rozpoznat, kolik zpráv mám smazat.")
+                        except (ValueError, IndexError):
+                            await message.channel.send("Nemohu rozpoznat, kolik zpráv mám smazat.")
 
             if not command_found:
                 await message.channel.send(f"{message.author.mention} tento příkaz neznám..")
