b1161022d571963aa25709fabc9d663c7d1cd7e2
Kousek_CZ <jan.kus@post.cz>
2023-11-02 11:51:48 +0100

    update new file

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
-import asyncio
-import datetime
+from manageServer import manage_server
 
 intents = discord.Intents.all()
 intents.presences = True
@@ -21,66 +20,8 @@ async def on_ready():
 
 
 @bot.event
-async def on_message(message): # message.author.mention
-    global target_channel
-    try:
-        now = datetime.datetime.now()
-        date_time = now.strftime("%Y-%m-%d %H:%M:%S")
-        target_channel = bot.get_channel(log_channel)
-        list_of_commands = ["help", "ahoj", "pingg", "smaž"]
-        command_found = False
-        server = message.guild
-
-        if bot.user.mentioned_in(message) and "@everyone" not in message.content:
-            content = message.content.lower()
-
-            for command in list_of_commands:
-                if command in content:
-                    command_found = True
-
-                    match command:
-                        case "help":
-                            await message.channel.send(
-                                '__# Seznam příkazů:__\npro aktivaci napiš @Kusovník [název příkazu]\n\n- help => zobrazí seznam příkazů\n- ahoj => vypíše uvítací zprávu')
-                            await target_channel.send(f"```diff\n+ {date_time} - Na serveru '{server}' byl použit příkaz '{command}'.\n```")
-
-                        case "ahoj":
-                            await message.channel.send(f'{message.author.mention} Co mě pinguješ more...')
-                            await target_channel.send(f"```diff\n+ {date_time} - Na serveru '{server}' byl použit příkaz '{command}'.\n```")
-
-                        case "pingg":
-                            for i in range(3):
-                                await message.channel.send("@everyone")
-                            await target_channel.send(f"```diff\n+ {date_time} - Na serveru '{server}' byl použit příkaz '{command}'.\n```")
-
-                        case "smaž":
-                            await target_channel.send(f"```diff\n+ {date_time} - Na serveru '{server}' byl použit příkaz '{command}'.\n```")
-                            try:
-                                words = content.split()
-                                smaz_index = words.index("smaž")
-                                num_to_delete = int(words[smaz_index + 1])
-                                messages_to_delete = []
-
-                                async for previous_message in message.channel.history(limit=num_to_delete + 1):
-                                    messages_to_delete.append(previous_message)
-
-                                if messages_to_delete:
-                                    await message.channel.delete_messages(messages_to_delete)
-
-                                    result_message = await message.channel.send(f"Smazal jsem {num_to_delete} zpráv...")
-
-                                    await asyncio.sleep(2)
-                                    await result_message.delete()
-                                else:
-                                    await message.channel.send("Nemohu najít zprávy ke smazání.")
-
-                            except (ValueError, IndexError):
-                                await message.channel.send("Nemohu rozpoznat, kolik zpráv mám smazat.")
-
-            if not command_found:
-                await message.channel.send(f"{message.author.mention} tento příkaz neznám..")
-    except Exception as e:
-        await target_channel.send(f"<@!481879980612124703>```diff\n- ERROR: Chyba při zpracování zprávy u příkazu '{command}' na serveru '{server}': {e}\n```")
+async def on_message(message):
+    await manage_server(bot, message, log_channel)
 
 
 try:
